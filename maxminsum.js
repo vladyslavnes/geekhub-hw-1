819e933ee2f5b47a84f9716f76ca1c7fa1a192a8
Vladyslav Nesterenko <easterenko@gmail.com>
2017-10-09 11:54:14 +0300

    Update maxminsum.js

--- maxminsum.js
+++ maxminsum.js
@@ -2,7 +2,7 @@ function maxminsum (argArr) {
   var arr = argArr, // for function not to affect the real array
       sum = 0;
   for (var i = 0; i < arr.length; i++) {
-    if (Number.isInteger(arr[i])) { // if number
+    if (Number.isInteger(arr[i]) || arr[i].toString().indexOf('.') != -1) { // if number is either integer or float
       sum += arr[i]; // increase sum
     }
     else arr.splice(i,1); // otherwise, remove element from array
