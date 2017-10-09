3d5e70d436feed7e9961dd410eb0eeba0f2cc94f
Vladyslav Nesterenko <easterenko@gmail.com>
2017-10-09 11:44:42 +0300

    Improved solution

--- maxminsum.js
+++ maxminsum.js
@@ -1,24 +1,27 @@
-function maxminsum (arr) {
-  var max = 0,
-      min = 0,
+function maxminsum (argArr) {
+  var arr = argArr, // for function not to affect the real array
       sum = 0;
-  for (var i = 0;i < arr.length;i++) {
-    if (typeof arr[i] === 'number' && !(isNaN(arr[i]))) { // check if this element is a number (and not NaN)
-      sum += arr[i];
-      
-      if (arr[i] > max) // is given number bigger than previous max?
-        max = arr[i];
-      
-      if (arr[i] < min) // is given number less than previous min?
-        min = arr[i];
+  for (var i = 0; i < arr.length; i++) {
+    if (Number.isInteger(arr[i])) { // if number
+      sum += arr[i]; // increase sum
     }
+    else arr.splice(i,1); // otherwise, remove element from array
   }
-  return {max,min,sum}; //ES6 stuff (instead of {max:max,min:min,sum:sum})
+  console.log(arr);
+  
+  var max = Math.max.apply(Math,arr),
+      min = Math.min.apply(Math,arr);
+
+  return {
+    max,
+    min,
+    sum
+  };
 }
 
 // test
 
-var test = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+var test = [undefined,-2,-1];
 console.log(`
 Max number: ${maxminsum(test).max}
 Min Number: ${maxminsum(test).min}
