3df1c0d77edc59ab3c1af7b0f38b5aa6e2d3ef90
Vladyslav Nesterenko <easterenko@gmail.com>
2017-10-12 11:24:45 +0300

    Refactored maxminsum.js

--- maxminsum.js
+++ maxminsum.js
@@ -1,32 +1,22 @@
 function maxminsum (argArr) {
   var arr = argArr; // for function not to affect the real array
-
-  // filter all 'bad' values
-  for (var i = 0; i < arr.length; i++) {
-    if (
-        typeof arr[i] === 'undefined' ||
-        typeof arr[i] === 'string' ||
-        isNaN(arr[i])
-    )
-    // if number is neither integer nor float
-      arr.splice(i,1);
-  }
-
   var sum = 0,
       max = arr[0],
       min = arr[0];
   
   // now we have an array of just numbers (and Infinity)
   for (var i = 0; i < arr.length; i++) {
-    sum += arr[i]; // increase sum
+    if (typeof arr[i] === 'number' && !isNaN(arr[i]) {
+      sum += arr[i]; // increase sum
 
-    // find the greatest number
-    if (arr[i] > max)
-      max = arr[i];
+      // find the greatest number
+      if (arr[i] > max)
+        max = arr[i];
   
-    // filter the least number
-    if (arr[i] < min)
-      min = arr[i];
+      // filter the least number
+      if (arr[i] < min)
+        min = arr[i];
+    }
   }
 
   return {
